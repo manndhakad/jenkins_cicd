c5b98122dfdc05c541a11c4d0102f8c07e6370cc
manndhakad <mdhakad1189@gmail.com>
2025-09-17 13:45:04 +0530

    Delete iac-terraform/backend.tf

--- iac-terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "c3opsstate"                 # Replace with your bucket name
-    key     = "envs/dev/terraform.tfstate" # Path to state file in the bucket
-    region  = "ap-south-1"                 # Replace with your AWS region
-    encrypt = true                         # Encrypt the state file at rest
-    #dynamodb_table = "terraform-locks"                # (Optional) for state locking
-  }
-}
